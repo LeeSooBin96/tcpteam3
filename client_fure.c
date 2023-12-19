46669ad71cd6dab97c5b0a1d1ba4bca011108698
LeeSooBin96 <153247743+LeeSooBin96@users.noreply.github.com>
2023-12-19 10:24:47 +0900

    Add files via upload

--- client_fure.c
+++ client_fure.c
@@ -4,17 +4,26 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 #define BUF_SIZE 1024
 void error_handling(char *message);
+void* send_data(void* arg[2])
+{
+    write(arg[0], arg[1], BUF_SIZE);
+}
 
 int main(int argc, char* argv[])
 {
+    //ip, 포트번호 지정
+    argc=3; argv[1]="10.10.20.98"; argv[2]="9106";
     int sock;
     char message[BUF_SIZE];
     int filesz,file_len;
     FILE * file;
     struct sockaddr_in serv_addr;
     
+    pthread_t t_id;
+
     if(argc!=3)
     {
         printf("Usage: %s <IP> <PORT>\n",argv[0]);
@@ -35,13 +44,16 @@ int main(int argc, char* argv[])
 
     file=fopen("2013_01_.csv","rbwb");
     // filesz=sizeof(file);
-    while(feof(file)==0)
+    while(1)
     {
         fgets(message, BUF_SIZE, file);
-        write(sock, message, sizeof(message));
-        
+        if(feof(file)) break;
+        void* arg[2]={sock,message};
+        pthread_create(&t_id,NULL,send_data,arg);
+        pthread_detach(t_id);
     }
-
+    close(sock);
+    return 0;
 }
 
 void error_handling(char *message)
